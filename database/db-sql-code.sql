de44b37bca0520085992267b81e839db79033077
EskolaHJ <esk23003@byui.edu>
2025-01-24 15:02:11 -0700

    added query 4 and 6 from the assignment2.sql to the rebuild file

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -235,3 +235,15 @@ VALUES   (
     'White',
     5
   );
+
+
+-- Task 4: modify the 'GM Hummer' Record description
+UPDATE inventory
+	SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+	WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+
+-- Task 6: Update all inventory records to add "/vehicles" to the file path
+UPDATE inventory
+	SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+		inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
