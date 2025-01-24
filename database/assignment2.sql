e4d531f07d4d4c764d90d81432acbc2f23d6813e
EskolaHJ <esk23003@byui.edu>
2025-01-23 21:08:48 -0700

    fixed the sql code in this file

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,29 +1,29 @@
 -- Task 1: Insert a new record into the account table
-INSERT INTO account (first_name, last_name, email, password)
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starknet.com', 'Iam1ronM@n');
 
 --  Task 2: Update Tony Stark's account_type to "Admin"
 UPDATE account
-SET account_type = 'Admin'
-WHERE email = 'tony@starkent.com'
+	SET account_type = 'Admin'
+	WHERE account_email = 'tony@starknet.com';
 
--- Task 3: Delete the Tony Stark record from the database
+-- task 3: Delete the Tony Stark record from the database
 DELETE FROM account
-WHERE email = 'tony@starkent.com';
+	WHERE account_email = 'tony@starknet.com';
 
--- Task 4: Modify the "GM Hummer" Record description
+-- Task 4: modify the 'GM Hummer' Record description
 UPDATE inventory
-SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
-WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+	SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+	WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
 -- Task 5: Inner join to select make, model, and classification name from "Sport" category
 SELECT i.inv_make, i.inv_model, c.classification_name
-FROM inventory i 
-INNER JOIN classification c
-ON i.classification_id = c.classification_id
-WHERE c.classification_name = 'Sport',
+	FROM inventory i
+		INNER JOIN classification c
+			ON i.classification_id = c.classification_id
+				WHERE c.classification_name = 'Sport';
 
 -- Task 6: Update all inventory records to add "/vehicles" to the file path
 UPDATE inventory
-SET inv_img = REPLACE(inv_image, '/images/', '/images/vehicles/'),
-    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+	SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+		inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
