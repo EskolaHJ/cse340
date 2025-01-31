5560356cc6405145ea7f5e8e9c696b92223bcb42
EskolaHJ <esk23003@byui.edu>
2025-01-30 21:36:35 -0700

    fixed the inventory-model

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,5 +1,3 @@
-const invModel = require("../models/inventory-model");
-const Util = {};
 const pool = require("../database/");
 
 /* ***************************
@@ -15,87 +13,38 @@ async function getClassifications() {
     }
 }
 
+/* ***************************
+ *  Get inventory by classification ID
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+    try {
+        const data = await pool.query(
+            `SELECT * FROM public.inventory WHERE classification_id = $1`, [classification_id]
+        );
+        return data.rows;
+    } catch (error) {
+        console.error("getInventoryByClassificationId error:", error);
+        throw error;
+    }
+}
+
+/* ***************************
+ *  Get a specific vehicle by inventory ID
+ * ************************** */
+async function getVehicleById(inv_id) {
+    try {
+        const data = await pool.query(
+            `SELECT * FROM public.inventory WHERE inv_id = $1`, [inv_id]
+        );
+        return data.rows[0]; // Return a single vehicle object
+    } catch (error) {
+        console.error("getVehicleById error:", error);
+        throw error;
+    }
+}
+
 module.exports = {
-    getClassifications
+    getClassifications,
+    getInventoryByClassificationId,
+    getVehicleById
 };
-
-
-/* ************************
- * Constructs the nav HTML unordered list
- ************************** */
-Util.getNav = async function (req, res, next) {
-  let data = await invModel.getClassifications();
-  let list = "<ul>";
-  list += '<li><a href="/" title="Home page">Home</a></li>';
-  data.rows.forEach((row) => {
-    list += "<li>";
-    list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>";
-    list += "</li>";
-  });
-  list += "</ul>";
-  return list;
-};
-
-/* **************************************
- * Build the classification view HTML
- * ************************************ */
-Util.buildClassificationGrid = async function (data) {
-  let grid = "";
-
-  if (data.length > 0) {
-    grid = '<ul id="inv-display">';
-    data.forEach(vehicle => {
-      grid += '<li>';
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id
-        + '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model
-        + ' details"><img src="' + (vehicle.inv_image.startsWith('/') ? vehicle.inv_image : '/images/' + vehicle.inv_image)
-        + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + ' on CSE Motors" /></a>';
-      grid += '<div class="namePrice">';
-      grid += '<hr />';
-      grid += '<h2>';
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id + '" title="View '
-        + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">'
-        + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>';
-      grid += '</h2>';
-      grid += '<span>$'
-        + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>';
-      grid += '</div>';
-      grid += '</li>';
-    });
-    grid += '</ul>';
-  } else {
-    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>';
-  }
-
-  return grid;
-};
-
-/* **************************************
- * Build the vehicle detail HTML
- * ************************************ */
-Util.buildVehicleDetailHTML = function (vehicle) {
-  return `
-    <div class="vehicle-detail">
-        <h1>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h1>
-        <div class="vehicle-layout">
-            <img src="${vehicle.inv_image.startsWith('/') ? vehicle.inv_image : '/images/' + vehicle.inv_image}" 
-                 alt="${vehicle.inv_make} ${vehicle.inv_model}">
-            <div class="vehicle-info">
-                <p><strong>Price:</strong> $${new Intl.NumberFormat("en-US").format(vehicle.inv_price)}</p>
-                <p><strong>Mileage:</strong> ${new Intl.NumberFormat("en-US").format(vehicle.inv_miles)} miles</p>
-                <p><strong>Color:</strong> ${vehicle.inv_color}</p>
-                <p><strong>Description:</strong> ${vehicle.inv_description}</p>
-            </div>
-        </div>
-    </div>
-  `;
-};
-
-module.exports = Util;
